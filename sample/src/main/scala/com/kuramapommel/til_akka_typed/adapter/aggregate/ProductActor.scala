7415e67f2b417c6659a1e948d3ff8939bfd8c40b
kuramapommel <pommel.pg@gmail.com>
2024-09-29 19:09:47 +0900

    akka-persistence-typed の実装

--- sample/src/main/scala/com/kuramapommel/til_akka_typed/adapter/aggregate/ProductActor.scala
+++ sample/src/main/scala/com/kuramapommel/til_akka_typed/adapter/aggregate/ProductActor.scala
@@ -2,15 +2,23 @@ package com.kuramapommel.til_akka_typed.adapter.aggregate
 
 import akka.actor.typed.ActorRef
 import akka.actor.typed.Behavior
+import akka.actor.typed.scaladsl.ActorContext
 import akka.actor.typed.scaladsl.Behaviors
+import akka.persistence.typed.PersistenceId
+import akka.persistence.typed.scaladsl.Effect
+import akka.persistence.typed.scaladsl.EventSourcedBehavior
+import cats.data.EitherT
 import com.kuramapommel.til_akka_typed.domain.model.Product
 import com.kuramapommel.til_akka_typed.domain.model.ProductIdGenerator
 import com.kuramapommel.til_akka_typed.domain.model.ProductRepository
+import com.kuramapommel.til_akka_typed.domain.model.error.ProductError
 import com.kuramapommel.til_akka_typed.domain.model.event.ProductEvent
 import com.kuramapommel.til_akka_typed.domain.model.valueobject.*
 import com.kuramapommel.til_akka_typed.usecase.EditProductUseCaseImpl
 import com.kuramapommel.til_akka_typed.usecase.RegisterProductUseCaseImpl
 import scala.concurrent.ExecutionContext
+import scala.concurrent.Future
+import scala.util.Success
 
 /** 商品アクター. */
 object ProductActor:
@@ -59,6 +67,72 @@ object ProductActor:
           val productRepository = ProductActorRepository(Some(product), ctx)
           active(productRepository, idGenerator)
 
+        case Persist(event) =>
+          Behaviors.same
+
+  def apply(productId: ProductId)(using ctx: ActorContext[Command]): Behavior[Command] =
+    val commandHandler: (Option[Product], Command) => Effect[ProductEvent, Option[Product]] =
+      (productMaybe, command) =>
+        import Command.*
+        given executionContext: ExecutionContext =
+          ctx.system.executionContext
+        val repository = new ProductRepository:
+          override def findById(id: ProductId): ExecutionContext ?=> EitherT[Future, ProductError, Product] =
+            productMaybe match
+              case Some(product) if product.id == id =>
+                EitherT.rightT[Future, ProductError](product)
+              case _ =>
+                EitherT.leftT[Future, Product](ProductError.NotFound)
+
+          override def save(product: Product): ExecutionContext ?=> EitherT[Future, ProductError, ProductId] =
+            EitherT.rightT[Future, ProductError](product.id)
+
+        command match
+          case Register(name, imageUrl, price, description, replyTo) =>
+            val usecase = RegisterProductUseCaseImpl(ProductIdGenerator(() => productId), repository)
+            usecase
+              .execute(name, imageUrl, price, description): event =>
+                ctx.pipeToSelf(Future.successful(event)):
+                  case Success(event) => Command.Persist(event)
+                  case _              => ???
+            Effect.none
+
+          case Edit(id, replyTo, nameOpt, imageUrlOpt, priceOpt, descriptionOpt) =>
+            val usecase = EditProductUseCaseImpl(repository)
+            usecase
+              .execute(id, nameOpt, imageUrlOpt, priceOpt, descriptionOpt): event =>
+                ctx.pipeToSelf(Future.successful(event)):
+                  case Success(event) => Command.Persist(event)
+                  case _              => ???
+            Effect.none
+
+          case Store(product) =>
+            Effect.none
+
+          case Persist(event) => Effect.persist(event)
+
+    val eventHandler: (Option[Product], ProductEvent) => Option[Product] = (productMaybe, event) =>
+      event match
+        case ProductEvent.Registered(productId, name, imageUrl, price, description) =>
+          productMaybe.orElse(Some(Product(productId, name, imageUrl, price, description)))
+
+        case ProductEvent.Edited(productId, name, imageUrl, price, description) =>
+          productMaybe.map((product) =>
+            product.copy(
+              name = name.getOrElse(product.name),
+              imageUrl = imageUrl.getOrElse(product.imageUrl),
+              price = price.getOrElse(product.price),
+              description = description.getOrElse(product.description)
+            )
+          )
+
+    EventSourcedBehavior[Command, ProductEvent, Option[Product]](
+      persistenceId = PersistenceId.ofUniqueId(productId.value),
+      emptyState = None,
+      commandHandler = commandHandler,
+      eventHandler = eventHandler
+    )
+
 /** 商品アクターコマンド */
 enum Command:
   /**
@@ -67,6 +141,8 @@ enum Command:
    *   商品
    */
   case Store(product: Product)
+
+  case Persist(event: ProductEvent)
 
   /**
    * 登録.
