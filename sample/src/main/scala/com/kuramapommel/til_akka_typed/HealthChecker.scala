8284ae106ae280a49c30bf2c5d91c8aaaadee688
kuramapommel <pommel.pg@gmail.com>
2024-11-23 23:01:39 +0900

    k8s 向けヘルスチェッカーを追加

---
+++ sample/src/main/scala/com/kuramapommel/til_akka_typed/HealthChecker.scala
@@ -0,0 +1,17 @@
+package com.kuramapommel.til_akka_typed
+
+import akka.actor.typed.ActorSystem
+import scala.concurrent.Future
+
+/**
+ * k8s 向けヘルスチェッカー
+ * 参照：https://doc.akka.io/libraries/akka-management/current/healthchecks.html#defining-a-health-check
+ *
+ * @param system
+ *     アクターシステム
+ */
+final case class HealthChecker(system: ActorSystem[?]) extends (() => Future[Boolean]):
+
+  /** 稼働中 */
+  override def apply(): Future[Boolean] =
+    Future.successful(true)
